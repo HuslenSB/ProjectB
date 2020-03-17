f771a6f9e01830ec887275d6a0319e5e72aad607
HuslenSB <0990204@hr.nl>
2020-03-17 09:44:02 +0100

    Test

--- Test.cs
+++ Test.cs
@@ -1,16 +1,5 @@
 class program {
     public static void Main() {
-        int len = 5;
-        int[] array = new int[len];
-        int s=1;
-        for(int i = 0; i<len;i++){
-            if (s%2!=0){
-                array[i]=s;
-                s+=2;
-            }
-        }
-        string task = "update first and last";
-        array[0] = 99;
-        array[len-1] = 88;
+        //test
     }
 }
