998e82e5d59dcaa356a3b5965316a51c968d2000
HuslenSB <58810716+HuslenSB@users.noreply.github.com>
2020-04-14 10:05:52 +0200

    Update Form1.Designer.cs

--- Escaplication/Escaplication/Form1.Designer.cs
+++ Escaplication/Escaplication/Form1.Designer.cs
@@ -165,7 +165,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(922, 539);
+       
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
