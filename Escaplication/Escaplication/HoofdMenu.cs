3201e51f4ce9b9918b5469a5434e9a3fcb5cf1bb
jaspereck <60735321+jaspereck@users.noreply.github.com>
2020-04-21 11:00:54 +0200

    Update HoofdMenu.cs

--- Escaplication/Escaplication/HoofdMenu.cs
+++ Escaplication/Escaplication/HoofdMenu.cs
@@ -19,50 +19,50 @@ namespace Escaplication
         //Escape room informatie link
         private void Label1_Click(object sender, EventArgs e)
         {
-            Close();
+            //Close();
             //Code voor openen form Informatie
         }
 
         private void Informatie_Click(object sender, EventArgs e)
         {
-            Close();
+            //Close();
             //Code voor openen form Informatie
         }
         //Locatie en contactgegevens link
         private void LocatieContact_Click(object sender, EventArgs e)
         {
-            Close();
+            //Close();
             //Code voor openen form Contact
         }
 
         private void Contact_Click(object sender, EventArgs e)
         {
-            Close();
+            //Close();
             //Code voor openen form Contact
         }
         //Resereer link
         private void Reserveren_Click(object sender, EventArgs e)
         {
-            Close();
+            //Close();
             //Code voor openen form Reserveer
         }
 
         private void Reserveertekst_Click(object sender, EventArgs e)
         {
-            Close();
+            //Close();
             //Code voor openen form Reserveer
         }
 
         //Escape room Recensies
         private void Recensies_Click(object sender, EventArgs e)
         {
-            Close();
+            //Close();
             //Code voor openen form Recensies
         }
 
         private void RecensieText_Click(object sender, EventArgs e)
         {
-            Close();
+            //Close();
             //Code voor openen form Recensies
         }
     }
