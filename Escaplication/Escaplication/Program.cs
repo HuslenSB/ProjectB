abf32055cfac3e6504048ba793dcea4aa9026824
davidhr-dev <0993555@hr.nl>
2020-04-22 11:19:59 +0200

    Veranderd op welk scherm het programma opstart

--- Escaplication/Escaplication/Program.cs
+++ Escaplication/Escaplication/Program.cs
@@ -18,7 +18,7 @@ namespace Escaplication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Reserveren());
+            Application.Run(new HoofdMenu());
 
         }
     }
